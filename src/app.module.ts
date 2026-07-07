be6918a20a9fe3377b787badc754d12a5fcc15a6
jyx-07 <s25069@gsm.hs.kr>
2026-07-07 13:19:27 +0900

    add(global): BullMQ 연결 및 3D 프린터 파이프라인 모듈 통합

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
+import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OpenaiModule } from './openai/openai.module';
+import { OpenscadModule } from './openscad/openscad.module';
+import { PrinterModule } from './printer/printer.module';
+import { QueueModule } from './queue/queue.module';
+import { SlicerModule } from './slicer/slicer.module';
+import { FilesModule } from './files/files.module';
+import { MeshyModule } from './meshy/meshy.module';
+import { ThreeDModule } from './3d/3d.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        connection: {
+          host: configService.get<string>('REDIS_HOST', 'localhost'),
+          port: configService.get<number>('REDIS_PORT', 6379),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    OpenaiModule,
+    OpenscadModule,
+    SlicerModule,
+    PrinterModule,
+    QueueModule,
+    FilesModule,
+    MeshyModule,
+    ThreeDModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
