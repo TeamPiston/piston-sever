d9a45c49145f30d20c622f3777891a1eddc9f3ed
jyx-07 <s25069@gsm.hs.kr>
2026-07-07 11:57:57 +0900

    add(user): User 엔티티 및 UserModule 추가

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn({ type: 'bigint', unsigned: true, name: 'user_id' })
+  userId: string;
+
+  @Column({ name: 'login_id', type: 'varchar', length: 30, unique: true })
+  loginId: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+
+  @Column({ type: 'boolean', default: false })
+  verified: boolean;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'datetime',
+    precision: 3,
+    default: () => 'CURRENT_TIMESTAMP(3)',
+  })
+  createdAt: Date;
+}
