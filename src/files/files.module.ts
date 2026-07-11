bbef3897209022447210b3386d22bd83454beb2c
jyx-07 <s25069@gsm.hs.kr>
2026-07-11 20:40:10 +0900

    add(files): MinIO 클라이언트 및 업로드 기능 추가

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './application/files.service';
+import { minioClientProvider } from './infrastructure/minio-client.provider';
 
 /**
  * FilesService를 제공하고 다른 모듈에서 사용할 수 있도록 내보내는 모듈.
  */
 @Module({
-  providers: [FilesService],
+  providers: [minioClientProvider, FilesService],
   exports: [FilesService],
 })
 export class FilesModule {}
